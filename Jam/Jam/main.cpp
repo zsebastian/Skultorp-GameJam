3fe3de38ba3160f35b2278b3c892e1028c10c75c
Elias Furenhed <eliasaif@gmail.com>
2013-04-05 21:23:19 +0200

    Test

--- Jam/Jam/main.cpp
+++ Jam/Jam/main.cpp
@@ -5,7 +5,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+	int i;
     while (window.isOpen())
     {
         sf::Event event;

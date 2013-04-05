17c8405775893978f8d4010d32f0f6055b6c091a
Sebastian Zander <z.sebastian87@gmail.com>
2013-04-06 01:20:40 +0200

    tst

--- Jam/Jam/GravityField.cpp
+++ Jam/Jam/GravityField.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include "Ball.h"
 #include "Cat.h"
+#include "Utility.h"
 
 GravityField::GravityField(float gravityForce)
 	:mGravityForce(gravityForce)
@@ -11,31 +12,35 @@ GravityField::GravityField(float gravityForce)
 
 }
 
-void GravityField::addObject(std::shared_ptr<Ball> ball)
+void GravityField::addObject(std::shared_ptr<Entity> entity)
 {
-	mBalls.push_back(ball);
+	std::shared_ptr<Cat> cat = std::dynamic_pointer_cast<Cat>(entity);
+	std::shared_ptr<Ball> ball = std::dynamic_pointer_cast<Ball>(entity);
+
+	if (cat)
+		mCat = cat;
+	else if (ball)
+		mBalls.push_back(ball);
 }
 
-void GravityField::addObject(std::shared_ptr<Cat> cat)
+
+void GravityField::removeObject(std::shared_ptr<Entity> entity)
 {
-	mCat = cat;
+	std::shared_ptr<Cat> cat = std::dynamic_pointer_cast<Cat>(entity);
+	std::shared_ptr<Ball> ball = std::dynamic_pointer_cast<Ball>(entity);
+	
+	if (cat)
+	{
+		if (mCat == cat)
+			mCat = nullptr;
+	}
+	else if (ball)
+	{
+		auto pred = [&ball](std::shared_ptr<Ball> testball) {return testball.get() == ball.get();};
+		Util::eraseIf(mBalls, pred);
+	}
+	
 }
-
-void GravityField::removeObject(std::shared_ptr<Ball> ball)
-{
-	auto pred = [&ball](std::shared_ptr<Ball> testball) {return testball.get() == ball.get();};
-
-	mBalls.erase(
-		std::remove_if(mBalls.begin(), mBalls.end(), pred),
-		mBalls.end());
-}
-
-void GravityField::removeObject(std::shared_ptr<Cat> cat)
-{
-	if (mCat == cat)
-		mCat = nullptr;
-}
-
 
 void GravityField::update()
 {
@@ -44,27 +49,10 @@ void GravityField::update()
 	for (auto ball: mBalls)
 	{
 		//F = G(m1*m2)/r^2
-		float F = ((mGravityForce) * ball->getMass() * mCat->getMass()) / distance(ball->getPosition(), mCat->getPosition());
-		sf::Vector2f gravVector = F * normalize(ball->getPosition() - mCat->getPosition());
+		float F = ((mGravityForce) * ball->getMass() * mCat->getMass()) / Util::distance(ball->getPosition(), mCat->getPosition());
+		sf::Vector2f gravVector = F * Util::normalize(ball->getPosition() - mCat->getPosition());
 		sumGravVector += gravVector;
 	}
 
 	mCat->setGravityVector(sumGravVector);
-}
-
-float GravityField::distance(sf::Vector2f v0, sf::Vector2f v1)
-{
-	float dx = v0.x - v1.x;
-	float dy = v0.y - v1.y;
-	return std::abs(std::sqrt((dx*dx) + (dy*dy)));
-}
-
-sf::Vector2f GravityField::normalize(sf::Vector2f v)
-{
-	return (v * (1 / length(v)));
-}
-
-float GravityField::length(sf::Vector2f v)
-{
-	return std::abs(std::sqrt((v.x*v.x) + (v.y*v.y)));
 }
--- Jam/Jam/GravityField.h
+++ Jam/Jam/GravityField.h
@@ -13,11 +13,9 @@ class GravityField
 {
 public:
 	GravityField(float gravityForce);
-	void addObject(std::shared_ptr<Ball>);
-	void addObject(std::shared_ptr<Cat>);
+	void addObject(std::shared_ptr<Entity>);
 
-	void removeObject(std::shared_ptr<Ball>);
-	void removeObject(std::shared_ptr<Cat>);
+	void removeObject(std::shared_ptr<Entity>);
 
 	void update();
 
@@ -26,9 +24,6 @@ private:
 	std::shared_ptr<Cat> mCat;
 	float mGravityForce;
 
-	float distance(sf::Vector2f, sf::Vector2f);
-	sf::Vector2f normalize(sf::Vector2f);
-	float length(sf::Vector2f);
 };
 
 #endif

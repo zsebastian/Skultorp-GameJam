8b9425d5be5aea949cd3c14153c7872766000cfd
Elias Furenhed <eliasaif@gmail.com>
2013-04-05 23:40:39 +0200

    Cat

--- Jam/Jam/Cat.cpp
+++ Jam/Jam/Cat.cpp
@@ -3,6 +3,7 @@
 
 Cat::Cat()
 	:mMass(0.f)
+	,mGravityVector(0.f, 0.f)
 {
 	mTempTexture.loadFromFile("data/cat.png");
 	mTempSprite.setTexture(mTempTexture);
@@ -25,7 +26,8 @@ void Cat::setMass(float mass)
 
 void Cat::update()
 {
-
+	//Apply gravity
+	mPosition += mGravityVector;
 }
 
 void Cat::render(Display& display)
